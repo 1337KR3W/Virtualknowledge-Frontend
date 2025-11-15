4a7713ead24905659a114ee246435121955cce0e
josrojrom1 <josrojrom@alum.us.es>
2025-11-15 13:39:50 +0100

    login-1 with Material Design working fine!

--- src/app/components/login-1/login-1.ts
+++ src/app/components/login-1/login-1.ts
@@ -6,26 +6,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-login-1',
-  standalone: true,
   imports: [
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './login-1.html',
   styleUrl: './login-1.scss',
 })
 export class Login1Page {
-  loginForm: FormGroup;
+  loginForm1: FormGroup;
   loginError: string = '';
 
 
@@ -35,9 +37,36 @@ export class Login1Page {
     private router: Router,
     private authService: AuthService
   ) {
-    this.loginForm = this.fb.group({
+    this.loginForm1 = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
+  }
+
+  onLogin() {
+    console.log('--- 1. FUNCIÓN onLogin DISPARADA ---'); // <-- Añade esto
+    this.loginError = '';
+
+    if (this.loginForm1.valid) {
+      console.log('--- 2. FORMULARIO VÁLIDO ---'); // <-- Añade esto
+      const { email, password } = this.loginForm1.value;
+      const loginSuccessful = this.authService.login(email, password);
+
+      if (loginSuccessful) {
+        console.log('--- 3. LOGIN ÉXITO, REDIRECCIONANDO ---');
+        console.log('Success login, redirecting...');
+        this.router.navigate(['/home']);
+      } else {
+        console.log('--- 4. LOGIN FALLIDO ---');
+        this.loginError = 'Incorrect email or password. Please, try again';
+      }
+    } else {
+      console.log('--- 5. FORMULARIO INVÁLIDO ---');
+      this.loginError = 'Complete all fields, please'
+    }
+  }
+
+  goToRegister() {
+    this.router.navigate(['/register']);
   }
 }
