7f9a4acb5de241b04c9f8144660dc449e89b2750
josrojrom1 <josrojrom@alum.us.es>
2025-11-16 14:00:03 +0100

    Added style and inconsto MD cards in welcome page

--- src/app/components/md-components/card-1/card-1.ts
+++ src/app/components/md-components/card-1/card-1.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 interface Feature {
   title: string;
   description: string;
   cssClass: string;
-  imagePath: string;
+  iconName: string;
 }
 @Component({
   selector: 'app-card-1',
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './card-1.html',
   styleUrl: './card-1.scss',
 })
@@ -21,20 +22,20 @@ export class Card1Component {
       title: 'Secure Management',
       description: 'Robust registration and login with route protection (Auth Guard).',
       cssClass: 'card-bg-lock',
-      imagePath: "assets/images/original/card-bg-lock.png"
+      iconName: 'lock'
     },
     {
       title: 'Neumorphic Design',
       description: 'Modern and unique aesthetic, with shadows that simulate depth and interactive elements.',
       cssClass: 'card-bg-palette',
-      imagePath: 'assets/images/original/card-bg-palette.png'
+      iconName: 'palette'
     },
 
     {
       title: 'Consistent UX',
       description: 'Optimized user flow with global navbar and footer that adapt to your session.',
       cssClass: 'card-bg-layout',
-      imagePath: 'assets/images/original/card-bg-layout.png'
+      iconName: 'web'
 
     },
 
@@ -42,21 +43,21 @@ export class Card1Component {
       title: 'Total Adaptability',
       description: 'Interface compatible with desktop and mobile devices thanks to relative units.',
       cssClass: 'card-bg-responsive',
-      imagePath: 'assets/images/original/card-bg-responsive.png'
+      iconName: 'view_carousel'
     },
 
     {
       title: 'Modular Code',
       description: 'Structure based on standalone components and SCSS variables for easy maintenance.',
       cssClass: 'card-bg-code',
-      imagePath: 'assets/images/original/card-bg-code.png'
+      iconName: 'space_dashboard'
     },
 
     {
       title: 'Material Design',
       description: 'Latest version of Material Design M3.',
       cssClass: 'card-bg-hover',
-      imagePath: 'assets/images/original/card-bg-hover.png'
+      iconName: 'design_services'
     },
   ];
 
