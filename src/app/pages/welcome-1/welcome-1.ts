8df9cce1dbaeb01a2a5481203fe6248a5b21d7b0
josrojrom1 <josrojrom@alum.us.es>
2025-11-16 14:36:03 +0100

    New style in all pages: centered vertical forms

--- src/app/pages/welcome-1/welcome-1.ts
+++ src/app/pages/welcome-1/welcome-1.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Card1Component } from '../../components/md-components/card-1/card-1';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-welcome-1',
-  imports: [CommonModule, RouterModule, Card1Component],
+  imports: [CommonModule, RouterModule, Card1Component, MatCardModule],
   templateUrl: './welcome-1.html',
   styleUrl: './welcome-1.scss',
 })
