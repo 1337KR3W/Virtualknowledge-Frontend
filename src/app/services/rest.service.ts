67be95bb6495e13dba337951231d2004e7f90ef3
josrojrom1 <josrojrom@alum.us.es>
2026-04-10 12:55:41 +0200

    Avances en rest service y entornos, aun condigurando el proyecto

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,35 +1,53 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { PersistenceService } from './persistence.service';
+import { AbstractService } from './abstract.service';
 
 @Injectable({ providedIn: 'root' })
-export class RestService {
-  private readonly http = inject(HttpClient);
+export class RestService extends AbstractService {
+
   private readonly persistence = inject(PersistenceService);
 
-  private getHeaders() {
-    let headers = new HttpHeaders({
-      'X-API-KEY': environment.apiKey,
-      'X-API-SECRET': environment.apiSecret
-    });
+  private path: string | null = null;
 
-    const token = this.persistence.getToken();
-    if (token) {
-      headers = headers.set('Authorization', `Bearer ${token}`);
+  private async getPath(): Promise<any> {
+    console.log('[Rest.getPath] INICIO'); //BORRAR LOG
+    try {
+      if (this.path !== null) {
+        console.log('[Rest.getPath] Usando basePathCache:', this.path); //BORRAR LOG
+        return this.path;
+      }
+
+      const customer = await this.persistence.getValue('customer');
+      console.log('[Rest.getPath] customer obtenido:', customer); //BORRAR LOG
+
+      this.path = customer
+        ? environment.urlPrefix.replace('$CUSTOMER$', customer) + environment.apiUrl
+        : '';
+
+      console.log('[Rest.getPath] basePathCache generado:', this.path); //BORRAR LOG
+      console.log('[Rest.getPath] FIN'); //BORRAR LOG
+
+      return this.path;
+    } catch (error) {
+      console.error('[restService][getPath]', error);
     }
-    return headers;
+
   }
 
-  post<T>(endpoint: string, body: any) {
-    return this.http.post<T>(`${environment.apiUrl}${endpoint}`, body, {
-      headers: this.getHeaders()
-    });
-  }
 
-  get<T>(endpoint: string) {
-    return this.http.get<T>(`${environment.apiUrl}${endpoint}`, {
-      headers: this.getHeaders()
-    });
+  async login(username: string, password: string): Promise<any> {
+    try {
+      const basePath = await this.getPath();
+      console.log('[Rest.login] POST →', `${basePath}authenticate`, { username }); //BORRAR LOG
+
+      return this.makePostRequestWithoutHeaders(`${basePath}authenticate`, {
+        username,
+        password
+      });
+    } catch (error) {
+      console.error('[restService][login]', error);
+    }
+
   }
 }
